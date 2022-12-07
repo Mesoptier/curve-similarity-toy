99ba5e8c6b87a762c4a45913e31457ded311b8ec
Koen Klaren <koen.klaren@gmail.com>
2022-12-07 19:20:00 +0100

    Experiment some more with wasm-bindgen

--- rs_lib/src/lib.rs
+++ rs_lib/src/lib.rs
@@ -5,6 +5,33 @@ pub fn add(a: i32, b: i32) -> i32 {
     return a + b;
 }
 
+#[wasm_bindgen]
+pub fn sum(items: &[i32]) -> i32 {
+    items.iter().sum()
+}
+
+#[wasm_bindgen]
+pub fn max(items: &[i32]) -> Option<i32> {
+    items.iter().max().copied()
+}
+
+#[wasm_bindgen]
+pub enum NumberEnum {
+    Foo = 0,
+    Bar = 1,
+    Qux = 2,
+}
+
+#[wasm_bindgen]
+pub fn get_enum(name: &str) -> Option<NumberEnum> {
+    match name {
+        "foo" => Some(NumberEnum::Foo),
+        "bar" => Some(NumberEnum::Bar),
+        "qux" => Some(NumberEnum::Qux),
+        _ => None,
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
