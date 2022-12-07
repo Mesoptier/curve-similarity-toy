95e3de2c961885bff6202639b85ca935e2d2d6bb
Koen Klaren <koen.klaren@gmail.com>
2022-12-07 16:43:45 +0100

    Start switching to regular yarn+esbuild

--- main.ts
+++
@@ -1,7 +0,0 @@
-import { instantiate } from './lib/rs_lib.generated.js';
-
-instantiate({
-    url: new URL('rs_lib_bg.wasm', document.location.href),
-}).then(({ add }) => {
-    console.log(add(1, 2));
-});
