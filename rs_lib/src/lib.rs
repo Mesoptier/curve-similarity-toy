84cc3179b776c10547bbf975e5ed45f599a611bc
Koen Klaren <koen.klaren@gmail.com>
2022-12-10 13:26:12 +0100

    Render triangle from Rust using WebGL
    Using https://rustwasm.github.io/wasm-bindgen/examples/webgl.html

--- rs_lib/src/lib.rs
+++ rs_lib/src/lib.rs
@@ -1,44 +1,133 @@
 use wasm_bindgen::prelude::*;
+use web_sys::{WebGl2RenderingContext, WebGlProgram, WebGlShader};
 
 #[wasm_bindgen]
-pub fn add(a: i32, b: i32) -> i32 {
-    return a + b;
+pub fn start(
+    context: &WebGl2RenderingContext,
+    width: u32,
+    height: u32,
+) -> Result<(), JsValue> {
+    let vert_shader = compile_shader(
+        &context,
+        WebGl2RenderingContext::VERTEX_SHADER,
+        r##"#version 300 es
+
+        in vec4 position;
+
+        void main() {
+            gl_Position = position;
+        }
+        "##,
+    )?;
+
+    let frag_shader = compile_shader(
+        &context,
+        WebGl2RenderingContext::FRAGMENT_SHADER,
+        r##"#version 300 es
+
+        precision highp float;
+        out vec4 outColor;
+
+        void main() {
+            outColor = vec4(1, 1, 0.5, 1);
+        }
+        "##,
+    )?;
+
+    let program = link_program(&context, &vert_shader, &frag_shader)?;
+    context.use_program(Some(&program));
+
+    // TODO: These are vec3's (right?), could they just be vec2's instead?
+    let vertices: [f32; 9] = [-0.7, -0.7, 0.0, 0.7, -0.7, 0.0, 0.0, 0.7, 0.0];
+
+    let position_attribute_location =
+        context.get_attrib_location(&program, "position");
+    let buffer = context.create_buffer().ok_or("Failed to create buffer")?;
+    context.bind_buffer(WebGl2RenderingContext::ARRAY_BUFFER, Some(&buffer));
+
+    unsafe {
+        let positions_array_buf_view = js_sys::Float32Array::view(&vertices);
+        context.buffer_data_with_array_buffer_view(
+            WebGl2RenderingContext::ARRAY_BUFFER,
+            &positions_array_buf_view,
+            WebGl2RenderingContext::STATIC_DRAW,
+        )
+    }
+
+    let vao = context
+        .create_vertex_array()
+        .ok_or("Could not create vertex array object")?;
+    context.bind_vertex_array(Some(&vao));
+
+    context.vertex_attrib_pointer_with_i32(
+        position_attribute_location as u32,
+        3,
+        WebGl2RenderingContext::FLOAT,
+        false,
+        0,
+        0,
+    );
+    context.enable_vertex_attrib_array(position_attribute_location as u32);
+
+    // TODO: Why is this called a second time?
+    context.bind_vertex_array(Some(&vao));
+
+    // Draw
+    context.clear_color(0.0, 0.0, 0.0, 1.0);
+    context.clear(WebGl2RenderingContext::COLOR_BUFFER_BIT);
+
+    let vert_count = (vertices.len() / 3) as i32;
+    context.draw_arrays(WebGl2RenderingContext::TRIANGLES, 0, vert_count);
+
+    Ok(())
 }
 
-#[wasm_bindgen]
-pub fn sum(items: &[i32]) -> i32 {
-    items.iter().sum()
-}
+fn compile_shader(
+    context: &WebGl2RenderingContext,
+    shader_type: u32,
+    source: &str,
+) -> Result<WebGlShader, String> {
+    let shader = context
+        .create_shader(shader_type)
+        .ok_or_else(|| "Unable to create shader object".to_string())?;
+    context.shader_source(&shader, source);
+    context.compile_shader(&shader);
 
-#[wasm_bindgen]
-pub fn max(items: &[i32]) -> Option<i32> {
-    items.iter().max().copied()
-}
-
-#[wasm_bindgen]
-pub enum NumberEnum {
-    Foo = 0,
-    Bar = 1,
-    Qux = 2,
-}
-
-#[wasm_bindgen]
-pub fn get_enum(name: &str) -> Option<NumberEnum> {
-    match name {
-        "foo" => Some(NumberEnum::Foo),
-        "bar" => Some(NumberEnum::Bar),
-        "qux" => Some(NumberEnum::Qux),
-        _ => None,
+    if context
+        .get_shader_parameter(&shader, WebGl2RenderingContext::COMPILE_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(shader)
+    } else {
+        Err(context
+            .get_shader_info_log(&shader)
+            .unwrap_or_else(|| "Unknown error compiling shader".to_string()))
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn link_program(
+    context: &WebGl2RenderingContext,
+    vert_shader: &WebGlShader,
+    frag_shader: &WebGlShader,
+) -> Result<WebGlProgram, String> {
+    let program = context
+        .create_program()
+        .ok_or_else(|| "Unable to create program object".to_string())?;
 
-    #[test]
-    fn it_works() {
-        let result = add(1, 2);
-        assert_eq!(result, 3);
+    context.attach_shader(&program, vert_shader);
+    context.attach_shader(&program, frag_shader);
+    context.link_program(&program);
+
+    if context
+        .get_program_parameter(&program, WebGl2RenderingContext::LINK_STATUS)
+        .as_bool()
+        .unwrap_or(false)
+    {
+        Ok(program)
+    } else {
+        Err(context
+            .get_program_info_log(&program)
+            .unwrap_or_else(|| "Unknown error linking program".to_string()))
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,10 @@
-import init, {add, get_enum, max, NumberEnum, sum} from '../rs_lib/pkg';
+import init, { start } from '../rs_lib/pkg';
 // @ts-ignore: esbuild is configured to export the filename of the .wasm file
 import wasmFilePath from '../rs_lib/pkg/rs_lib_bg.wasm';
 
 init(new URL(wasmFilePath, import.meta.url)).then(() => {
-    console.log(add(1, 2));
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('webgl2');
 
-    const items = new Int32Array([1, 2, 3, 4]);
-    console.assert(sum(items) === 10);
-    console.assert(max(items) === 4);
-    console.assert(max(new Int32Array([])) === undefined);
-    console.assert(get_enum('foo') === NumberEnum.Foo);
-    console.assert(get_enum('bar') === NumberEnum.Bar);
-    console.assert(get_enum('qux') === NumberEnum.Qux);
-    console.assert(get_enum('asdf') === undefined);
+    start(ctx, 800, 800);
 });
-
-console.log('Test');
