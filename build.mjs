2b6f83bd83dc1dd74625716a497293993b5e2cb2
Koen Klaren <koen.klaren@gmail.com>
2022-12-31 12:43:23 +0100

    Explicitly set dev/release profile

--- build.mjs
+++ build.mjs
@@ -2,15 +2,18 @@ import esbuild from 'esbuild';
 
 const watch = process.argv.includes('--watch');
 
+const profile = process.argv.includes('--dev') ? 'dev' : 'release';
+
 esbuild.build({
     entryPoints: ['./src/main.tsx'],
     outdir: 'www/build',
-    format: "esm",
+    format: 'esm',
     bundle: true,
     watch,
     logLevel: 'info',
     loader: {
-        '.wasm': 'file'
+        '.wasm': 'file',
     },
-    sourcemap: 'linked'
+    sourcemap: 'linked',
+    minify: profile === 'release',
 });
