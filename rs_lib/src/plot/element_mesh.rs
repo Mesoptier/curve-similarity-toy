e2dbe84c4ae7d6e3af43c64e8ffa573e5bb1de6d
Koen Klaren <koen.klaren@gmail.com>
2023-01-11 20:28:42 +0100

    Add connectivity map

--- rs_lib/src/plot/element_mesh.rs
+++ rs_lib/src/plot/element_mesh.rs
@@ -32,6 +32,9 @@ pub struct ElementMesh<Value> {
     vertices: Vec<Vertex<Value>>,
     // Isosceles triangles, with corner indices in clockwise order, with base first
     triangle_elements: Vec<[usize; 3]>,
+    // Maps from edge to connected edge (where edges are `(vertex_idx, edge_idx)` pairs) or None if
+    // the edge is on the mesh boundary.
+    connectivity: Vec<[Option<(usize, usize)>; 3]>,
 }
 
 impl<Value> ElementMesh<Value> {
@@ -64,18 +67,29 @@ impl<Value> ElementMesh<Value> {
             0..(x_points.len() - 1),
             0..(y_points.len() - 1),
         )
-        .flat_map(|bl| {
-            // Indices of the corners of this quad
-            let bl = bl.1 + bl.0 * y_points.len();
+        .flat_map(|(x_idx, y_idx)| {
+            // Get indices of the vertices at the corners of the quad:
+            // tl---tr
+            // |     |
+            // |     |
+            // bl---br
+            let bl = y_idx + x_idx * y_points.len();
             let tl = bl + 1;
             let br = bl + y_points.len();
             let tr = br + 1;
 
-            // tl---tr
-            // |   ╱ |
-            // | ╱   |
-            // bl---br
-            [[bl, tr, br], [tr, bl, tl]]
+            [
+                //      tr
+                //     ╱ |
+                //   ╱   |
+                // bl---br
+                [bl, tr, br],
+                // tl---tr
+                // |   ╱
+                // | ╱
+                // bl
+                [tr, bl, tl],
+            ]
         })
         .collect_vec();
 
@@ -83,9 +97,96 @@ impl<Value> ElementMesh<Value> {
         let num_triangles = num_quads * 2;
         assert_eq!(triangle_elements.len(), num_triangles);
 
+        // Build connectivity map
+        let connectivity = triangle_elements
+            .iter()
+            .enumerate()
+            .map(|(triangle_idx, &elements)| {
+                // Offsets to next triangle idx
+                let offset_horizontal = 2 * (y_points.len() - 1) + 1;
+                let offset_vertical = 1;
+
+                if triangle_idx % 2 == 0 {
+                    let [_, _, br] = elements;
+                    let br_x_idx = br / y_points.len();
+                    let br_y_idx = br % y_points.len();
+
+                    [
+                        Some((triangle_idx + 1, 0)),
+                        if br_x_idx < x_points.len() - 1 {
+                            Some((triangle_idx + offset_horizontal, 1))
+                        } else {
+                            None
+                        },
+                        if br_y_idx > 0 {
+                            Some((triangle_idx - offset_vertical, 2))
+                        } else {
+                            None
+                        },
+                    ]
+                } else {
+                    let [_, _, tl] = elements;
+                    let tl_x_idx = tl / y_points.len();
+                    let tl_y_idx = tl % y_points.len();
+
+                    [
+                        Some((triangle_idx - 1, 0)),
+                        if tl_x_idx > 0 {
+                            Some((triangle_idx - offset_horizontal, 1))
+                        } else {
+                            None
+                        },
+                        if tl_y_idx < y_points.len() - 1 {
+                            Some((triangle_idx + offset_vertical, 2))
+                        } else {
+                            None
+                        },
+                    ]
+                }
+            })
+            .collect_vec();
+
+        // Assert that each edge in the connectivity mapping corresponds to the same edge in the
+        // connected triangle
+        debug_assert!(connectivity.iter().enumerate().all(
+            |(triangle_idx, connections)| {
+                connections
+                    .iter()
+                    .enumerate()
+                    .all(|(edge_idx, connection)| {
+                        connection.map_or(
+                            true,
+                            |(other_triangle_idx, other_edge_idx)| {
+                                [
+                                    triangle_elements[triangle_idx][edge_idx],
+                                    triangle_elements[triangle_idx]
+                                        [(edge_idx + 1) % 3],
+                                ] == [
+                                    triangle_elements[other_triangle_idx]
+                                        [(other_edge_idx + 1) % 3],
+                                    triangle_elements[other_triangle_idx]
+                                        [other_edge_idx],
+                                ]
+                            },
+                        )
+                    })
+            }
+        ));
+
+        // Assert that there is the correct number of boundary edges
+        debug_assert_eq!(
+            connectivity
+                .iter()
+                .flatten()
+                .filter(|c| c.is_none())
+                .count(),
+            2 * (x_points.len() - 1) + 2 * (y_points.len() - 1)
+        );
+
         Self {
             vertices,
             triangle_elements,
+            connectivity,
         }
     }
 
@@ -103,5 +204,17 @@ impl<Value> ElementMesh<Value> {
 
     pub fn triangle_elements(&self) -> &Vec<[usize; 3]> {
         &self.triangle_elements
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::plot::element_mesh::ElementMesh;
+
+    #[test]
+    fn from_points() {
+        let x_points = vec![0., 1., 2.];
+        let y_points = vec![0., 1., 2.];
+        ElementMesh::from_points((&x_points, &y_points), |_| 0.);
     }
 }
